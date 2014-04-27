cc993a7055e6890e8f42192843abd0f3f0f008c2
Minh Luu <mluu510@gmail.com>
2014-04-26 18:42:44 -0700

    window boundaries added

--- Labyrinth/ViewController.m
+++ Labyrinth/ViewController.m
@@ -27,9 +27,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    CGPoint center = CGPointMake(self.view.bounds.size.width/2, self.view.bounds.size.height/2);
     CGSize ballSize = CGSizeMake(30, 30);
-    CGRect frame = CGRectMake(center.x - ballSize.width/2, center.y - ballSize.height/2, ballSize.width, ballSize.height);
+    CGRect frame = CGRectMake(self.view.center.x - ballSize.width/2, self.view.center.y - ballSize.height/2, ballSize.width, ballSize.height);
     self.ball = [[BallView alloc] initWithFrame:frame];
     
     [self.view addSubview:self.ball];
@@ -59,11 +58,30 @@
     self.xSpeed += 2 * dx;
     self.ySpeed += 2 * dy;
     
+        [self bounceOffWalls];
+    
     self.ball.frame = CGRectMake(self.ball.frame.origin.x + self.xSpeed, self.ball.frame.origin.y - self.ySpeed, self.ball.frame.size.width, self.ball.frame.size.height);
+    
+
     
     self.xSpeed /= 1.1;
     self.ySpeed /= 1.1;
 
+}
+
+- (void)bounceOffWalls
+{
+    if (self.ball.frame.origin.x <= 0) {
+        self.xSpeed = fabs(self.xSpeed/1.1);
+    } else if (self.ball.frame.origin.x >= self.view.bounds.size.width - self.ball.frame.size.width) {
+        self.xSpeed = -fabs(self.xSpeed/1.1);
+    }
+    
+    if (self.ball.frame.origin.y <= 0) {
+        self.ySpeed = -fabs(self.ySpeed/1.1);
+    } else if (self.ball.frame.origin.y >= self.view.bounds.size.height - self.ball.frame.size.height) {
+        self.ySpeed = fabs(self.ySpeed/1.1);
+    }
 }
 
 - (void)didReceiveMemoryWarning
